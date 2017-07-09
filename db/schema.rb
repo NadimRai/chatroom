950d854bf73d1826b2b2542a663d7225bbab3e82
NadimRai <rai_nadim@yahoo.com>
2017-07-08 22:49:27 -0700

    generated Message model with user_id room_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709054541) do
+ActiveRecord::Schema.define(version: 20170709054826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "room_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -29,4 +39,6 @@ ActiveRecord::Schema.define(version: 20170709054541) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
