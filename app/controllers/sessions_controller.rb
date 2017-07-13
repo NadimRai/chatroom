dbe30ec4b26255eab56aebb0ba56e102fc71dbed
NadimRai <rai_nadim@yahoo.com>
2017-07-13 07:47:02 -0700

    created create and destroy method for Session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,22 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		if user.try(:authenticate, params[:password])
+			session[:current_user] = user.id 
+			redirect_to root_url
+		else
+			redirect_to login_url 
+		end
 	end
 
 	def destroy 
+		session[:current_user] = nil 
+		redirect_to login_url
+	end
+
+	private
+
+	def user
+		@user = User.find_by_email(params[:email])
 	end
 end
